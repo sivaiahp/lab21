e73f5cb59d6bd3d1db0a29e6b7712cf9331d2ccb
Sivaiah Polamreddy <sivaiahp970@gmail.com>
2021-06-17 12:25:35 +0000

    first continues integration

---
+++ src/main/java/com/devops/lab21/devopspractice/DevopsPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.devops.lab21.devopspractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devops/lab21/devopspractice/DevopsPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devops.lab21.devopspractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
